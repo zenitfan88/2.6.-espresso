83f4f4d775bebdf66679d06468b68bc0c24eb465
zenitfan88 <nikanenkov@rambler.ru>
2023-04-04 20:51:52 +0400

    fixed test idling

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/GalleryIdlingTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/GalleryIdlingTest.java
@@ -6,11 +6,25 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.Espresso;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -37,7 +51,6 @@ public class GalleryIdlingTest {
     }
 
 
-
     @Test
     public void galleryIdlingTest() {
         ViewInteraction appCompatImageButton = onView(
@@ -48,6 +61,10 @@ public class GalleryIdlingTest {
         ViewInteraction galleryButton = onView(withId(R.id.nav_gallery));
         galleryButton.check(matches(isDisplayed()));
         galleryButton.perform(click());
+
+        ViewInteraction numberElement = onView(
+                allOf(withId(R.id.item_number), withText("7")));
+        numberElement.check(matches(withText("7")));
 
         ViewInteraction recyclerView = onView(withId(R.id.recycle_view));
         recyclerView.check(matches(CustomViewMatcher.recyclerViewSizeMatcher(10)));
--- app/src/main/java/ru/kkuzmichev/simpleappforespresso/EspressoIdlingResources.java
+++ app/src/main/java/ru/kkuzmichev/simpleappforespresso/EspressoIdlingResources.java
@@ -6,9 +6,11 @@ import androidx.test.espresso.idling.CountingIdlingResource;
 public class EspressoIdlingResources {
     private static final String RESOURCE = "GLOBAL";
     public static CountingIdlingResource idlingResource = new CountingIdlingResource(RESOURCE);
+
     public static void increment() {
         idlingResource.increment();
     }
+
     public static void decrement() {
         if(!idlingResource.isIdleNow()) {
             idlingResource.decrement();
