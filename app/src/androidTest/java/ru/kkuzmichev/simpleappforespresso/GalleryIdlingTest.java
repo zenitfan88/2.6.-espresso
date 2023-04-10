815d2d427f1517fe8c0beec03fb05e1ee5ba37f2
zenitfan88 <nikanenkov@rambler.ru>
2023-04-10 20:13:08 +0400

    fixed idling test

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/GalleryIdlingTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/GalleryIdlingTest.java
@@ -3,28 +3,19 @@ package ru.kkuzmichev.simpleappforespresso;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.hamcrest.Matchers.allOf;
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
+import androidx.appcompat.widget.AppCompatImageButton;
 
-import androidx.test.espresso.Espresso;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.ViewInteraction;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.rule.ActivityTestRule;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -53,8 +44,7 @@ public class GalleryIdlingTest {
 
     @Test
     public void galleryIdlingTest() {
-        ViewInteraction appCompatImageButton = onView(
-                withContentDescription("Open navigation drawer"));
+        ViewInteraction appCompatImageButton = onView(isAssignableFrom(AppCompatImageButton.class));
         appCompatImageButton.check(matches(isDisplayed()));
         appCompatImageButton.perform(click());
 
@@ -62,11 +52,13 @@ public class GalleryIdlingTest {
         galleryButton.check(matches(isDisplayed()));
         galleryButton.perform(click());
 
+        ViewInteraction recyclerView = onView(withId(R.id.recycle_view));
+        recyclerView.check(CustomViewAssertions.isRecyclerView());
+        recyclerView.check(matches(CustomViewMatcher.recyclerViewSizeMatcher(10)));
+
         ViewInteraction numberElement = onView(
                 allOf(withId(R.id.item_number), withText("7")));
         numberElement.check(matches(withText("7")));
 
-        ViewInteraction recyclerView = onView(withId(R.id.recycle_view));
-        recyclerView.check(matches(CustomViewMatcher.recyclerViewSizeMatcher(10)));
     }
 }
